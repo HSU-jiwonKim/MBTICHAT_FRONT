b3d868d032613f224aaa27f8beb8d76f99d5178b
JIWONKIM <4526as@naver.com>
2024-10-04 01:37:21 +0900

    Add socket connection logs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import "./App.css";
+import { useEffect } from "react";
+import socket from "./server"; // 소켓 들고오기
 
 function App() {
-  return (
-    <div>
-      <div className="App"></div>
-    </div>
-  );
+    useEffect(() => {
+        // 소켓 연결 상태 확인
+        socket.on("connect", () => {
+            console.log("Socket connected:", socket.id); // 소켓 ID 로그
+        });
+        
+        socket.on("disconnect", () => {
+            console.log("Socket disconnected");
+        });
+
+        return () => {
+            socket.off("connect");
+            socket.off("disconnect");
+        };
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Real-time Chat Application</h1>
+        </div>
+    );
 }
 
 export default App;
