8273576e4f5620557ec25b51ab5ac50ca4bb6c98
JIWONKIM <4526as@naver.com>
2024-10-10 13:46:07 +0900

    uiUpdate

--- src/components/MessageContainer/MessageContainer.js
+++ src/components/MessageContainer/MessageContainer.js
@@ -14,14 +14,8 @@ const MessageContainer = ({ messageList = [], user, userCount }) => {
         <div className="message-container-wrapper">
             <h5 className="user-count">그룹채팅 <span className="user-count-number">{userCount}</span></h5>
             <div className="message-scroll-container">
-                {messageList.map((message, index) => {
+                {messageList.map((message) => {
                     const userName = message?.user?.name || "Unknown";
-                    const messageTime = new Date(message.createdAt);
-                    const currentTime = new Date();
-                    const timeDifference = (currentTime - messageTime) / 1000; // 초 단위로 계산
-
-                    // 시간 표시 여부 결정
-                    const showTime = index === 0 || (timeDifference >= 60 && messageList[index - 1]?.user?.name === userName); // 첫 번째 메시지 또는 1분 이상 경과한 경우, 이전 메시지가 같은 사용자일 때만 시간 표시
 
                     return (
                         <Container key={message._id} className="message-container">
@@ -31,14 +25,12 @@ const MessageContainer = ({ messageList = [], user, userCount }) => {
                                 </div>
                             ) : userName === user?.name ? (
                                 <div className="my-message-container">
-                                    {showTime && (
-                                        <div className="timestamp2">
-                                            {messageTime.toLocaleTimeString([], {
-                                                hour: '2-digit',
-                                                minute: '2-digit',
-                                            })}
-                                        </div>
-                                    )}
+                                    <div className="timestamp2">
+                                        {new Date(message.createdAt).toLocaleTimeString([], {
+                                            hour: '2-digit',
+                                            minute: '2-digit',
+                                        })}
+                                    </div>
                                     <div className="my-message">{message.chat}</div>
                                 </div>
                             ) : (
@@ -52,14 +44,12 @@ const MessageContainer = ({ messageList = [], user, userCount }) => {
                                         <div className="user-name">{userName}</div>
                                         <div className="your-message">{message.chat}</div>
                                     </div>
-                                    {showTime && (
-                                        <div className="timestamp">
-                                            {messageTime.toLocaleTimeString([], {
-                                                hour: '2-digit',
-                                                minute: '2-digit',
-                                            })}
-                                        </div>
-                                    )}
+                                    <div className="timestamp">
+                                        {new Date(message.createdAt).toLocaleTimeString([], {
+                                            hour: '2-digit',
+                                            minute: '2-digit',
+                                        })}
+                                    </div>
                                 </div>
                             )}
                         </Container>
