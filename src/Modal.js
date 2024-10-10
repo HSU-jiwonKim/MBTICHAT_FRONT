3c99bfd065f5c5d47a22384d442960113a6f7d67
JIWONKIM <4526as@naver.com>
2024-10-10 21:58:33 +0900

    second commit

--- src/Modal.js
+++ src/Modal.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import './Modal.css'; // 스타일 파일 추가
 
-const Modal = ({ onClose, onSubmit }) => {
+const Modal = ({ onSubmit, onClose }) => {
     const [inputValue, setInputValue] = useState('');
 
     const handleSubmit = () => {
         if (inputValue) {
-            onSubmit(inputValue);
+            onSubmit(inputValue); // 입력된 값 전송
             onClose(); // 모달 닫기
         } else {
             alert('이름을 입력해야 합니다.'); // 유효성 검사
@@ -24,7 +24,6 @@ const Modal = ({ onClose, onSubmit }) => {
                     placeholder="당신의 이름을 입력하세요"
                 />
                 <button onClick={handleSubmit}>확인</button>
-                <button onClick={onClose}>취소</button>
             </div>
         </div>
     );
